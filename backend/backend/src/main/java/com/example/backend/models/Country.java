eaccb800a072c0a3ea41cbb775176eaf9b818e7c
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-04-10 01:32:19 +0300

    lr5_done

--- backend/backend/src/main/java/com/example/backend/models/Country.java
+++ backend/backend/src/main/java/com/example/backend/models/Country.java
@@ -17,6 +17,6 @@ public class Country {
     @Column(name = "id", updatable = false, nullable = false)
     public long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, unique = true)
     public String name;
 }
